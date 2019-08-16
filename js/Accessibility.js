a0454f6929df5183457af02e9ab0001a4a482d5a
RodrigoDisselli <disselli.rodrigo@gmail.com>
2019-08-15 22:08:21 -0300

    adição de link, botão pontilhado, hover para botões e sombra dos cards

---
+++ js/Accessibility.js
@@ -0,0 +1,11 @@
+// function addTitleLink(){
+//     links =  document.getElementsByTagName("a");
+
+//     for (var i = 0; i < links.length; i++){
+//         links[i].title = links[i].textContent;
+//     }
+// }
+
+// window.onload = function(){
+//    addTitleLink();
+// }
