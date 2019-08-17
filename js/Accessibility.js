9168808014c880da1f16dcdbc12e11a55b245ed3
RodrigoDisselli <disselli.rodrigo@gmail.com>
2019-08-17 18:32:56 -0300

    adicção de select correção do card e do input

--- js/Accessibility.js
+++ js/Accessibility.js
@@ -1,11 +1,14 @@
-// function addTitleLink(){
-//     links =  document.getElementsByTagName("a");
+// adiciona title automaticamente a todos os links na tela
+Array.from(document.getElementsByTagName("a")).forEach(links => {links.title = links.textContent});
 
-//     for (var i = 0; i < links.length; i++){
-//         links[i].title = links[i].textContent;
-//     }
-// }
+Array.from(document.getElementsByClassName("input")).forEach(inputs => {
+    console.log(inputs.innerHTML)
+    // inputs.innerHTML.from(document.getElementsByTagName("label")).forEach(addIdInput); console.log("rodou")});
+});
+// Array.from(document.getElementsByClassName("input")).forEach((inputs) => {
+//     inputs.from(document.getElementsByTagName("label")[this]).forEach(() => { console.log("inputs:" + inputs)})
+// })
 
-// window.onload = function(){
-//    addTitleLink();
-// }
+function addIdInput() {
+    console.log("Adiciona input");
+}
