546f706d75dd87a04a5ae7e3778279af26a6252a
RodrigoDisselli <disselli.rodrigo@gmail.com>
2019-08-17 20:37:15 -0300

    adição de controle das cores e bordas, criação da biblioteca de componentes

--- js/Accessibility.js
+++ js/Accessibility.js
@@ -1,14 +1,2 @@
 // adiciona title automaticamente a todos os links na tela
 Array.from(document.getElementsByTagName("a")).forEach(links => {links.title = links.textContent});
-
-Array.from(document.getElementsByClassName("input")).forEach(inputs => {
-    console.log(inputs.innerHTML)
-    // inputs.innerHTML.from(document.getElementsByTagName("label")).forEach(addIdInput); console.log("rodou")});
-});
-// Array.from(document.getElementsByClassName("input")).forEach((inputs) => {
-//     inputs.from(document.getElementsByTagName("label")[this]).forEach(() => { console.log("inputs:" + inputs)})
-// })
-
-function addIdInput() {
-    console.log("Adiciona input");
-}
