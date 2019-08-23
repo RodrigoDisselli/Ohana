bdbda371d80e3b922d49e95356f7b9973aa08ec2
RodrigoDisselli <disselli.rodrigo@gmail.com>
2019-08-23 18:22:49 -0300

    atualização do minify1.0

--- js/defineStyle.js
+++ js/defineStyle.js
@@ -1,35 +1,34 @@
 function primaryColor(color) {
     document.documentElement.style
-    .setProperty('--primary-color', color);
+        .setProperty('--primary-color', color);
 }
 
-function borderRadius(border){
+function borderRadius(border) {
     document.documentElement.style
-    .setProperty('--border-radius', (border + "px"));
+        .setProperty('--border-radius', (border + "px"));
 }
 
-function background(color){
+function background(color) {
     document.documentElement.style
-    .setProperty('--background-color', color);
+        .setProperty('--background-color', color);
 }
 
-function textColor(color){
+function textColor(color) {
     document.documentElement.style
-    .setProperty('--text-color', color);
+        .setProperty('--text-color', color);
 }
 
-function scrollStyle(color, width){
+function scrollStyle(color, width) {
     document.documentElement.style
-    .setProperty('--scroll-color', color);
+        .setProperty('--scroll-color', color);
     document.documentElement.style
-    .setProperty('--scroll-width', (width + "px"));
+        .setProperty('--scroll-width', (width + "px"));
 }
 
-function defineStyle(primaryColor, borderRadius, backgroundColor, textColor, scrollStyleColor, scrollStyleWidth){
+function defineStyle(primaryColor, borderRadius, backgroundColor, textColor, scrollStyleColor, scrollStyleWidth) {
     primaryColor(primaryColor); //default = #0590FF
     borderRadius(borderRadius); //default = 4
     backgroundColor(backgroundColor); //default = #FFFFFF
     textColor(textColor); //default = #333333
-    scrollStyleColor(scrollStyleColor); //default = #AAAAAA
-    scrollStyleWidth(scrollStyleWidth); //default = 10
+    scrollStyleColor(scrollStyleColor, scrollStyleWidth); //default = #AAAAAA | default = 10
 }
