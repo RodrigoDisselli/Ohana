8baafcff9f63aae4d2d7b738de64b701aca928bb
RodrigoDisselli <disselli.rodrigo@gmail.com>
2019-08-22 22:09:08 -0300

    adição de scroll estilizado e função global de alteração do tema

--- js/defineStyle.js
+++ js/defineStyle.js
@@ -1,12 +1,11 @@
-function color(color) {
+function primaryColor(color) {
     document.documentElement.style
     .setProperty('--primary-color', color);
 }
 
-function border(border){
-    border = border + "px";
+function borderRadius(border){
     document.documentElement.style
-    .setProperty('--border-radius', border);
+    .setProperty('--border-radius', (border + "px"));
 }
 
 function background(color){
@@ -17,4 +16,20 @@ function background(color){
 function textColor(color){
     document.documentElement.style
     .setProperty('--text-color', color);
+}
+
+function scrollStyle(color, width){
+    document.documentElement.style
+    .setProperty('--scroll-color', color);
+    document.documentElement.style
+    .setProperty('--scroll-width', (width + "px"));
+}
+
+function defineStyle(primaryColor, borderRadius, backgroundColor, textColor, scrollStyleColor, scrollStyleWidth){
+    primaryColor(primaryColor); //default = #0590FF
+    borderRadius(borderRadius); //default = 4
+    backgroundColor(backgroundColor); //default = #FFFFFF
+    textColor(textColor); //default = #333333
+    scrollStyleColor(scrollStyleColor); //default = #AAAAAA
+    scrollStyleWidth(scrollStyleWidth); //default = 10
 }
