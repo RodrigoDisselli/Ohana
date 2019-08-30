7ffda30598f93232ba018a07fd5432202aef790f
Rodrigo Disselli <disselli.rodrigo@gmail.com>
2019-08-30 13:58:58 -0300

    adição de lógica ao formulário

--- Ohana_ThemeBuilder/js/getAnswer.js
+++ Ohana_ThemeBuilder/js/getAnswer.js
@@ -1,62 +1,70 @@
-var inputData = document.getElementsByClassName("input-data");
+// var inputData = document.getElementsByClassName("input-data");
 
-Array.from(inputData).forEach(() => {
-    addEventListener("keypress", (event) => {
-        if(event.keyCode == 13){
-            event.preventDefault();
-            console.log("preenchido");
-        }else{
-            console.log("nada");
-        }
-    });
+// Array.from(inputData).forEach(() => {
+//     addEventListener("keypress", (event) => {
+//         if(event.keyCode == 13){
+//             event.preventDefault();
+//             console.log("preenchido");
+//         }else{
+//             console.log("nada");
+//         }
+//     });
+// });
+
+var inputPrimaryColor = document.getElementById("input-primary-color");
+var inputBackgroundColor = document.getElementById("input-background-color");
+var inputTextColor = document.getElementById("input-text-color");
+var inputBorderRadius = document.getElementById("input-border-radius");
+
+inputPrimaryColor.addEventListener("keypress", (event) => {
+    getKeyPress(event, "color", "input-primary-color", "background-color");
+    setTimeout(() => { primaryColor(inputPrimaryColor.value) }, 650);
 });
 
-// var inputPrimaryColor = document.getElementById("input-primary-color");
-// var inputBackgroundColor = document.getElementById("input-background-color");
-// var inputTextColor = document.getElementById("input-text-color");
-// var inputBorderRadius = document.getElementById("input-border-radius");
+inputBackgroundColor.addEventListener("keypress", (event) => {
+    getKeyPress(event, "color", "input-background-color", "text-color");
+    setTimeout(() => { backgroundColor(inputBackgroundColor.value) }, 650);
+});
 
-// inputPrimaryColor.addEventListener("keypress", (event) => {
-//     getKeyPress(event, "color", "input-primary-color", "background-color");
-// });
+inputTextColor.addEventListener("keypress", (event) => {
+    getKeyPress(event, "color", "input-text-color", "border-radius");
+    setTimeout(() => { textColor(inputTextColor.value) }, 650);
+});
 
-// inputBackgroundColor.addEventListener("keypress", (event) => {
-//     getKeyPress(event, "color", "input-background-color", "text-color");
-// });
+inputBorderRadius.addEventListener("keypress", (event) => {
+    getKeyPress(event, "number", "input-border-radius", "border-radius");
+    setTimeout(() => { borderRadius(inputBorderRadius.value) }, 650);
+});
 
-// inputTextColor.addEventListener("keypress", (event) => {
-//     getKeyPress(event, "color", "input-text-color", "border-radius");
-// });
+function fieldValidation(type, field, target){
+    if(type == "color"){
+        var value = document.getElementById(field).value;
+        if(String(value).includes("#") || String(value).includes("rgb(")){
+            goToScroll(target);
+            console.log("valido");
+        }
+        console.log("invalido")
+    }if(type == "number"){
+        if(value >= 0){
+            goToScroll(target);
+            console.log("valido");
+        }
+    }
+}
 
-// inputBorderRadius.addEventListener("keypress", (event) => {
-//     getKeyPress(event, "number", "input-border-radius", "border-radius");
-// });
+function getKeyPress(event, type, field, target){
+    if(event.keyCode == 13){
+        event.preventDefault();
+        fieldValidation(type, field, target);
+        console.log("enter = " + field);
+    }else{
+    }
+}
 
-// function fieldValidation(type, field, target){
-//     if(type == "color"){
-//         var value = document.getElementById(field).value;
-//         if(String(value).includes("#") || String(value).includes("rgb(")){
-//             goToScroll(target);
-//             console.log("valido");
-//         }
-//         console.log("invalido")
-//     }if(type == "number"){
-//         if(value >= 0){
-//             goToScroll(target);
-//             console.log("valido");
-//         }
-//     }
-// }
+function goToScroll(elementTarget){
+    var inputTarget = "input-" + elementTarget
+    document.getElementById(elementTarget).scrollIntoView({behavior: "smooth"});
+    setTimeout(() => { document.getElementById(inputTarget).focus(); }, 650);
 
-// function getKeyPress(event, type, field, target){
-//     if(event.keyCode == 13){
-//         event.preventDefault();
-//         fieldValidation(type, field, target);
-//         console.log("enter = " + field);
-//     }else{
-//     }
-// }
-
-// function goToScroll(target){
-//     document.getElementById(target).scrollIntoView({behavior: "smooth"});
-// }
+    console.log("focus");
+}
